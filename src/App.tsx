d580dd905985bdac7933c12949ef082968b91dfd
Dtheran <dtheran1@gmail.com>
2023-04-04 11:53:43 -0500

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Row, Col, Button, Stack } from 'react-bootstrap'
 import './App.css'
-import { ArrowsIcon } from './components/Icons'
+import { ArrowsIcon, ClipboardIcon, SpeakerIcon } from './components/Icons'
 import { LanguageSelector } from './components/LanguageSelector'
 import { AUTO_LANGUAGE } from './constants'
 import { useStore } from './hooks/useStore'
@@ -9,6 +9,8 @@ import { SectionType } from './types.d'
 import { TextArea } from './components/TextArea'
 import { useEffect } from 'react'
 import { translate } from './services/translate'
+import { useDebounce } from './hooks/useDebounce'
+import { VOICE_FOR_LANGUAGE } from './constants'
 
 function App () {
   const {
@@ -24,10 +26,12 @@ function App () {
     loading
   } = useStore()
 
-  useEffect(() => {
-    if (fromText === '') return
+  const debouncedFromText = useDebounce(fromText, 400)
 
-    translate({ fromLanguage, text: fromText, toLanguage })
+  useEffect(() => {
+    if (debouncedFromText === '') return
+
+    translate({ fromLanguage, text: debouncedFromText, toLanguage })
       .then(result => {
         if (result == null) return
         setResult(result)
@@ -35,7 +39,17 @@ function App () {
       .catch(() => {
         setResult('Error')
       })
-  }, [fromText, fromLanguage, toLanguage])
+  }, [debouncedFromText, fromLanguage, toLanguage])
+
+  const handleClipboard = () => {
+    navigator.clipboard.writeText(result).catch(() => {})
+  }
+  const handleSpeak = () => {
+    const utterance = new SpeechSynthesisUtterance(result)
+    utterance.lang = VOICE_FOR_LANGUAGE[toLanguage]
+    speechSynthesis.speak(utterance)
+  }
+
   return (
     <Container fluid>
       <h2>Google Translate Clone </h2>
@@ -72,12 +86,31 @@ function App () {
               value={toLanguage}
               onChange={setToLanguage}
             />
-            <TextArea
-              loading={loading}
-              value={result}
-              type={SectionType.To}
-              onChange={setResult}
-            />
+            <div style={{ position: 'relative' }}>
+              <TextArea
+                loading={loading}
+                value={result}
+                type={SectionType.To}
+                onChange={setResult}
+              />
+              <div style={{ position: 'absolute', left: 0, bottom: 0, display: 'flex' }}>
+                <Button
+                  variant='link'
+                  onClick={handleClipboard}
+                >
+                  {/* <ClipboardIcon /> */}
+                  <svg focusable="false" xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><g><rect fill="none" height="24" width="24"></rect></g><g><path d="M16,20H5V6H3v14c0,1.1,0.9,2,2,2h11V20z M20,16V4c0-1.1-0.9-2-2-2H9C7.9,2,7,2.9,7,4v12c0,1.1,0.9,2,2,2h9 C19.1,18,20,17.1,20,16z M18,16H9V4h9V16z"></path></g></svg>
+                </Button>
+                <Button
+                  variant='link'
+                  onClick={handleSpeak}
+                >
+                  {/* <SpeakerIcon /> */}
+
+                  <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path></svg>
+                </Button>
+              </div>
+            </div>
           </Stack>
         </Col>
       </Row>
